feb22db9ffa375395c64d9fda0e5f238cfe84d68
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-07-19 20:26:27 +0200

    Update EffectSizes_Tutors_BetweenGroups.R

--- hypotheses_test/EffectSizes_Tutors_BetweenGroups.R
+++ hypotheses_test/EffectSizes_Tutors_BetweenGroups.R
@@ -1,13 +1,32 @@
-"Effect Sizes of Between Groups Analyzes Stratified by Tutors
+
+"
+Effect Sizes of Between Groups Analyzes Stratified by Tutors
  Treatment: Tool-Based task
  Control: Paper-Based task
  Outcome Variables: Scores in Tasks 1 and 2, Answers from Questions 1 to 4
+
+Assumptions
+ Groups are independent (groups with different students and different tutors, but same task)
+ Groups have equal variance w.r.t. outcome variable (we used Levene test to verify that)
+ 
+ Method Wilcoxon Rank Sum Test which is executed for unpaired groups, because are independent groups.
+ R-package rstatix, function: wilcox.test(data, paired=FALSE)
+ 
+ We also compute the effect size using method wilcox_effsize in R-package rstatix by setting paired parameter to FALSE.
+ 
+ Package source: https://rpkgs.datanovia.com/rstatix/reference/wilcox_effsize.html
+ 
+ 
+ TODO:
+ - Check 
+ 
 "
 
 #install.packages("rstatix") #https://rpkgs.datanovia.com/rstatix/reference/wilcox_effsize.html
 #install.packages("coin")
 library(rstatix)
 library(coin)
+library(car)
 
 source("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//hypotheses_test//cleanup.r")
 
@@ -15,6 +34,8 @@ df1 <- cleanup_df1()
 
 tutor_list <- unique(df1$Tutor)[2:5] #removed first tutor as it has only row in the data (only treatment, no control)
 question_list <- colnames(df1[4:9]) #("score_1, score_2,question_1, question_2,question_3, question_4")
+
+
 
 "Compute rank sign test and effect sizes"
 compute_wilcox_tests <- function(df){
@@ -28,37 +49,37 @@ compute_wilcox_tests <- function(df){
     j=1
     df_tutor <-  df1[df1$Tutor==tutor,]
     
-    p_values[i,j] <- rstatix::pairwise_wilcox_test(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")$p
+    p_values[i,j] <- rstatix::wilcox_test(data=df_tutor,Score_1 ~ Treatment, paired=FALSE, alternative = "two.sided")$p
     r <- rstatix::wilcox_effsize(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")
     effect_size[i,j]<-r$effsize
     magnitude[i,j] <- levels(r$magnitude)[r$magnitude]
     j <- j+1
     
-    p_values[i,j] <- rstatix::pairwise_wilcox_test(data=df_tutor, Score_2 ~ Treatment, alternative = "two.sided")$p
+    p_values[i,j] <- rstatix::wilcox_test(data=df_tutor, Score_2 ~ Treatment, paired=FALSE, alternative = "two.sided")$p
     r <- rstatix::wilcox_effsize(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")
     effect_size[i,j]<-r$effsize
     magnitude[i,j] <- levels(r$magnitude)[r$magnitude]
     j <- j+1
     
-    p_values[i,j] <- rstatix::pairwise_wilcox_test(data=df_tutor, Question_1 ~ Treatment, alternative = "two.sided")$p
+    p_values[i,j] <- rstatix::wilcox_test(data=df_tutor, Question_1 ~ Treatment, paired=FALSE, alternative = "two.sided")$p
     r <- rstatix::wilcox_effsize(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")
     effect_size[i,j]<-r$effsize
     magnitude[i,j] <- levels(r$magnitude)[r$magnitude]
     j <- j+1 
     
-    p_values[i,j] <- rstatix::pairwise_wilcox_test(data=df_tutor, Question_2 ~ Treatment, alternative = "two.sided")$p
+    p_values[i,j] <- rstatix::wilcox_test(data=df_tutor, Question_2 ~ Treatment, paired=FALSE, alternative = "two.sided")$p
     r <- rstatix::wilcox_effsize(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")
     effect_size[i,j]<-r$effsize
     magnitude[i,j] <- levels(r$magnitude)[r$magnitude]
     j <- j+1 
     
-    p_values[i,j] <- rstatix::pairwise_wilcox_test(data=df_tutor, Question_3 ~ Treatment, alternative = "two.sided")$p
+    p_values[i,j] <- rstatix::wilcox_test(data=df_tutor, Question_3 ~ Treatment, paired=FALSE, alternative = "two.sided")$p
     r <- rstatix::wilcox_effsize(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")
     effect_size[i,j]<-r$effsize
     magnitude[i,j] <- levels(r$magnitude)[r$magnitude]
     j <- j+1 
    
-    p_values[i,j] <- rstatix::pairwise_wilcox_test(data=df_tutor, Question_4 ~ Treatment, alternative = "two.sided")$p
+    p_values[i,j] <- rstatix::wilcox_test(data=df_tutor, Question_4 ~ Treatment, paired=FALSE, alternative = "two.sided")$p
     r <- rstatix::wilcox_effsize(data=df_tutor, Score_1 ~ Treatment, alternative = "two.sided")
     effect_size[i,j]<-r$effsize
     magnitude[i,j] <- levels(r$magnitude)[r$magnitude]
@@ -82,9 +103,18 @@ compute_wilcox_tests <- function(df){
   return(out)
 }
 
-out <- compute_tests(df1)
+out <- compute_wilcox_tests(df1)
 
+#P-Values
+#    Score_1 Score_2 Question_1 Question_2 Question_3 Question_4
+#T2  0.1590   0.806      0.711      0.115     0.0987      0.167
+#T3  0.2220   0.282      0.282      1.000     0.4510      0.439
+#T4  0.0765   0.400      1.000      0.554     0.2800      0.629
+#T5  0.0636   0.582      0.696      0.693     0.4710      0.854
 
+out$effect_sizes
+
+out$magnitude
 
 
 
