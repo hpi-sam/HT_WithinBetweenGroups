7d505d0f46735f1cbd5405463d3f2c8e9ebf618b
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-07-19 21:01:16 +0200

    Corrected path to data loader

--- hypotheses_test/HT_Scores_E2.R
+++ hypotheses_test/HT_Scores_E2.R
@@ -1,7 +1,7 @@
 "
-Hypotheses tests on Experiment-2
+Hypotheses tests on Experiment-2 Between Group, hence independent groups, so using Wilcoxon rank sum test (paired=FALSE)
 "
-source("C://Users//Christian//Documents//SVN_Giese//ModelsRobotSimulator//hypotheses_test//cleanup.r")
+source("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//hypotheses_test//cleanup.r")
 
 df2 <- cleanup_df2()
 dim(df2)
@@ -27,22 +27,22 @@ shapiro.test(df2$Question_4)
 #Hypotheses tests
 
 #Score_1 Tool vs Paper
-df2_tool_2nd <-  df2[df2$Treament=="tool_2nd",]
+df2_tool_2nd <-  df2[df2$Treatment=="tool_2nd",]
 View(df2_tool_2nd)
 
-df2_paper_2nd <-  df2[df2$Treament=="paper_2nd",]
+df2_paper_2nd <-  df2[df2$Treatment=="paper_2nd",]
 View(df2_paper_2nd)
-wilcox.test(df2_tool_2nd$Score_1,df2_paper_2nd$Score_1,alternative = "two.sided") #W = 95, p-value = 0.07275
-wilcox.test(df2_tool_2nd$Score_1,df2_paper_2nd$Score_1,alternative = "less") #W = 95, p-value = 0.9684
-wilcox.test(df2_tool_2nd$Score_1,df2_paper_2nd$Score_1,alternative = "greater") #W = 95, p-value = 0.03637*********
+wilcox.test(df2_tool_2nd$Score_1,df2_paper_2nd$Score_1,paired=FALSE,alternative = "two.sided") #W = 95, p-value = 0.07275
+wilcox.test(df2_tool_2nd$Score_1,df2_paper_2nd$Score_1,paired=FALSE,alternative = "less") #W = 95, p-value = 0.9684
+wilcox.test(df2_tool_2nd$Score_1,df2_paper_2nd$Score_1,paired=FALSE,alternative = "greater") #W = 95, p-value = 0.03637*********
 
-wilcox.test(df2_tool_2nd$Score_2,df2_paper_2nd$Score_2,alternative = "two.sided") #W = 2, p-value = 0.8
-wilcox.test(df2_tool_2nd$Score_2,df2_paper_2nd$Score_2,alternative = "less") #W = 2, p-value = 0.4
-wilcox.test(df2_tool_2nd$Score_2,df2_paper_2nd$Score_2,alternative = "greater") #W = 2, p-value = 0.8
+wilcox.test(df2_tool_2nd$Score_2,df2_paper_2nd$Score_2,paired=FALSE,alternative = "two.sided") #W = 2, p-value = 0.8
+wilcox.test(df2_tool_2nd$Score_2,df2_paper_2nd$Score_2,paired=FALSE,alternative = "less") #W = 2, p-value = 0.4
+wilcox.test(df2_tool_2nd$Score_2,df2_paper_2nd$Score_2,paired=FALSE,alternative = "greater") #W = 2, p-value = 0.8
 
-wilcox.test(df2_tool_2nd$Question_1,df2_paper_2nd$Question_1,alternative = "two.sided") #W = 42, p-value = 0.128
-wilcox.test(df2_tool_2nd$Question_2,df2_paper_2nd$Question_2,alternative = "two.sided") #W = 64, p-value = 0.9224
-wilcox.test(df2_tool_2nd$Question_3,df2_paper_2nd$Question_3,alternative = "two.sided") #W = 76, p-value = 0.5565
-wilcox.test(df2_tool_2nd$Question_4,df2_paper_2nd$Question_4,alternative = "two.sided") #W = 48.5, p-value = 0.425
+wilcox.test(df2_tool_2nd$Question_1,df2_paper_2nd$Question_1,paired=FALSE,alternative = "two.sided") #W = 42, p-value = 0.128
+wilcox.test(df2_tool_2nd$Question_2,df2_paper_2nd$Question_2,paired=FALSE,alternative = "two.sided") #W = 64, p-value = 0.9224
+wilcox.test(df2_tool_2nd$Question_3,df2_paper_2nd$Question_3,paired=FALSE,alternative = "two.sided") #W = 76, p-value = 0.5565
+wilcox.test(df2_tool_2nd$Question_4,df2_paper_2nd$Question_4,paired=FALSE,alternative = "two.sided") #W = 48.5, p-value = 0.425
 
 
--- hypotheses_test/HT_Scores_paper1st_paper2nd.R
+++ hypotheses_test/HT_Scores_paper1st_paper2nd.R
@@ -1,7 +1,9 @@
 "
 Hypotheses tests on Experiment-1 paper_1st versus Experiment-2 paper_2nd
+Between groups, independent groups
+Wilcoxon Rank sum test (paired=FALSE)
 "
-source("C://Users//Christian//Documents//SVN_Giese//ModelsRobotSimulator//hypotheses_test//cleanup.r")
+source("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//hypotheses_test//cleanup.r")
 
 df1 <- cleanup_df1()
 df2 <- cleanup_df2()
@@ -11,18 +13,18 @@ df2 <- cleanup_df2()
 #Hypotheses tests
 
 #Score_1 paper vs Paper
-df1_paper_1st <-  df1[df1$Treament=="paper_1st",]
+df1_paper_1st <-  df1[df1$Treatment=="paper_1st",]
 View(df1_paper_1st)
 
-df2_paper_2nd <-  df2[df2$Treament=="paper_2nd",]
+df2_paper_2nd <-  df2[df2$Treatment=="paper_2nd",]
 View(df2_paper_2nd)
 
-wilcox.test(df1_paper_1st$Score_1,df2_paper_2nd$Score_1,alternative = "two.sided") #W = 125, p-value = 0.002051 ***********
-wilcox.test(df1_paper_1st$Score_1,df2_paper_2nd$Score_1,alternative = "less") #W = 125, p-value = 0.9992
-wilcox.test(df1_paper_1st$Score_1,df2_paper_2nd$Score_1,alternative = "greater") #W = 125, p-value = 0.001026 ***********
+wilcox.test(df1_paper_1st$Score_1,df2_paper_2nd$Score_1,paired=FALSE,alternative = "two.sided") #W = 125, p-value = 0.002051 ***********
+wilcox.test(df1_paper_1st$Score_1,df2_paper_2nd$Score_1,paired=FALSE,alternative = "less") #W = 125, p-value = 0.9992
+wilcox.test(df1_paper_1st$Score_1,df2_paper_2nd$Score_1,paired=FALSE,alternative = "greater") #W = 125, p-value = 0.001026 ***********
 #NOT COMPARABLE Question 2 between E1 and E2, so I am skpping it.
 
-wilcox.test(df1_paper_1st$Question_1,df2_paper_2nd$Question_1,alternative = "two.sided") #W = 76.5, p-value = 0.7749
-wilcox.test(df1_paper_1st$Question_2,df2_paper_2nd$Question_2,alternative = "two.sided") #W = 69, p-value = 0.9012
-wilcox.test(df1_paper_1st$Question_3,df2_paper_2nd$Question_3,alternative = "two.sided") #W = 100, p-value = 0.102
-wilcox.test(df1_paper_1st$Question_4,df2_paper_2nd$Question_4,alternative = "two.sided") #W = 67, p-value = 0.921
+wilcox.test(df1_paper_1st$Question_1,df2_paper_2nd$Question_1,paired=FALSE,alternative = "two.sided") #W = 76.5, p-value = 0.7749
+wilcox.test(df1_paper_1st$Question_2,df2_paper_2nd$Question_2,paired=FALSE,alternative = "two.sided") #W = 69, p-value = 0.9012
+wilcox.test(df1_paper_1st$Question_3,df2_paper_2nd$Question_3,paired=FALSE,alternative = "two.sided") #W = 100, p-value = 0.102
+wilcox.test(df1_paper_1st$Question_4,df2_paper_2nd$Question_4,paired=FALSE,alternative = "two.sided") #W = 67, p-value = 0.921
