f45c45454d14e1dd6e34b1a9355a4a49366766ea
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-07-20 22:02:30 +0200

    normalizing scores

--- hypotheses_test/cleanup.R
+++ hypotheses_test/cleanup.R
@@ -1,27 +1,35 @@
 
 
-
-cleanup_df1 <- function(){
-  df1 <-  read.csv("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//data//dataframe_1.csv")
+cleanup_task1 <- function(){
+  df1 <-  read.csv("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//data//task_1.csv")
   #Convert fields do numeric
   cols.num <- colnames(df1)[4:9]
   sapply(df1, class)
   df1[cols.num] <- sapply(df1[cols.num],as.numeric)
+  
+  #Convert score to percentages of the maximum score
+  df$Score_1 <- (df$Score_1/24 ) *100
+  df$Score_2 <- (df$Score_2/24 ) *100
   
   #Remove rows that have score either zero or NA
   df1 <- df1[!(is.na(df1$Score_1) & is.na(df1$Score_2)), ]
   
   #Replace all zeros for NA, so it does not distort the hypotheses tests
   df1[, 4:9][df1[, 4:9] == 0] <- NA 
+  
   return(df1)
 }
 
-cleanup_df2 <- function(){
-  df2 <-  read.csv("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//data//dataframe_2.csv")
+cleanup_task2 <- function(){
+  df2 <-  read.csv("C://Users//Christian//Documents//GitHub//HT_WithinBetweenGroups//data//task_2.csv")
   #Convert fields do numeric
   cols.num <- colnames(df2)[4:10]
   sapply(df2, class)
   df2[cols.num] <- sapply(df2[cols.num],as.numeric)
+  
+  #Convert score to percentages of the maximum score
+  df$Score_1 <- (df$Score_1/18 ) *100
+  df$Score_2 <- (df$Score_2/18 ) *100
   
   #Remove rows that have score either zero or NA
   df2 <- df2[!( (is.na(df2$Score_1) | df2$Score_1==0) & (is.na(df2$Score_2) | df2$Score_2==0) ), ]
